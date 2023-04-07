4a8f8f2d05b93ce759942b7b26addda00d4f82b3
Gnusarov Vlad <gnusarovvladislav@gmail.com>
2023-04-07 10:34:52 +0300

    Добавлено меню с выходом из аккаунта

--- app/src/main/java/com/example/forum/MainActivity.java
+++ app/src/main/java/com/example/forum/MainActivity.java
@@ -1,28 +1,24 @@
 package com.example.forum;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
-import android.widget.Toast;
-
 import com.example.forum.API.ApiInterface;
 import com.example.forum.API.RequestBuilder;
-import com.example.forum.Models.Users;
 import com.example.forum.fragments.FriendsFragment;
 import com.example.forum.fragments.TopicsFragment;
 import com.example.forum.fragments.YourProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -91,5 +87,27 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_layout, fragment);
         ft.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        int idItem = item.getItemId();
+        switch(idItem){
+            case R.id.exit:
+                exit();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void exit(){
+        Intent intent = new Intent(getApplicationContext(), Autorization.class);
+        startActivity(intent);
+        finish();
     }
 }
